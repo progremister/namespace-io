ce385efc0382fd031c45c58180ea3136f93132c1
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2024-10-30 16:15:10 +0100

    added a new document action

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +20,8 @@ export default function RootLayout({
         <body>
           <Header />
           <div className="flex min-h-screen">
-            {/* Sidebar */}
+            <Sidebar />
+
             <div className="flex-1 p-4 bg-gray-100 overflow-y-auto scrollbar-hide">
               {children}
             </div>
--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,11 +1,27 @@
-import { Button } from "./ui/button"
+"use client";
+
+import { useTransition } from "react";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { createNewDocument } from "@/actions/actions";
 
 function NewDocumentButton() {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  const handleCreateDocument = () => {
+    startTransition(async () => {
+      // new document
+      const { documentId } = await createNewDocument();
+      router.push(`/doc/${documentId}`);
+    });
+  };
+
   return (
     <div>
-      <Button>New Document</Button>
+      <Button onClick={handleCreateDocument} disabled={isPending}>{isPending ? "Creating..." : "New Document"}</Button>
     </div>
-  )
+  );
 }
 
-export default NewDocumentButton
+export default NewDocumentButton;
